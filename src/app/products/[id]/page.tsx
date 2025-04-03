848d1afba3e262b69544d86cb00b840ba1fe91d3
Trang Ha Viet <viettrangha@gmail.com>
2025-04-03 17:33:25 +0700

    refactor(ui): update product detail page to use prisma database

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,48 +1,58 @@
-import { Button } from "@/components/ui/button";
-import { dummyProducts } from '@/lib/data';
-import Image from "next/image";
+import AddToCartBtn from '@/components/Product/AddToCartBtn'
+import { prisma } from '@/server/prisma'
+import Image from "next/image"
 
-const ProductDetailPage = async (props: { params: Promise<{ id: string }> }) => {
-  const params = await props.params
+const ProductDetailPage = async ({ params }: { params: { id: string } }) => {
   const id = params.id
 
-  const product = dummyProducts.find((p) => p.id === id);
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+    },
+  })
 
   if (!product) {
-    return <div>Product not found.</div>;
+    return <div>Product not found.</div>
   }
 
   return (
     <div className="p-4">
       <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2 relative h-96 mb-4 md:mb-0">
+        <div className="md:w-1/3 relative h-96 mb-4 md:mb-0">
           <Image
             src={product.imageUrl}
-            alt={product.productName}
+            alt={product.name}
             layout="fill"
             objectFit="cover"
-            className="rounded-md"
+            className="rounded-md max-w-[432px]"
           />
         </div>
-        <div className="md:w-1/2 md:pl-8">
-          <h1 className="text-2xl font-bold mb-2">{product.productName}</h1>
-          <p className="text-xl font-semibold mb-4">${product.price.toFixed(2)}</p>
+        <div className="md:w-2/3 md:pl-8">
+          <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+          <p className="text-xl font-semibold mb-4">
+            ${product.price}
+          </p>
           <p className="text-gray-600 mb-4">{product.description}</p>
-          <Button className="w-full md:w-auto">Add to Cart</Button>
+          <AddToCartBtn id={product.id} className='md:w-auto'></AddToCartBtn>
         </div>
       </div>
       <div className="mt-4">
-        <h2 className="text-lg font-semibold mb-2">Related Images</h2>
         <div className="flex space-x-2">
           {product.images.map((img, index) => (
             <div key={index} className="relative h-20 w-20">
-              <Image src={img} alt={`Related ${index}`} layout="fill" objectFit="cover" className="rounded-md" />
+              <Image
+                src={img}
+                alt={`Related ${index}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-md"
+              />
             </div>
           ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductDetailPage;
+export default ProductDetailPage
